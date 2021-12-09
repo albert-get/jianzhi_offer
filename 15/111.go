7fd00fb3ce29b0ba439ee3ebcb42204a0ab506fa
章焱槟 <wxzhangyanbing@gfpartner.com.cn>
2021-12-09 18:11:29 +0800

    图

--- 15/111.go
+++ 15/111.go
@@ -1,4 +1,7 @@
 package main
+
+import "harry.com/jianzhi_offer/set"
+
 /**
 计算除法
  */
@@ -10,4 +13,38 @@ package main
 返回 所有问题的答案 。如果存在某个无法确定的答案，则用 -1.0 替代这个答案。如果问题中出现了给定的已知条件中没有出现的字符串，也需要用 -1.0 替代这个答案。
 
  */
+
+func calcEquation(equations [][]string,values []float32,queries [][]string) []float32 {
+	graph:=buildGraph(equations,values)
+	results:=make([]float32,len(queries))
+	for i:=0;i<len(queries);i++{
+		from:=queries[i][0]
+		to:=queries[i][1]
+		_,okFrom:=graph[from]
+		_,okTo:=graph[to]
+		if !okFrom||!okTo{
+			results[i]=-1
+		}else {
+			visited:=set.NewSet()
+			results[i]=dfs1(graph,from,to,visited)
+		}
+	}
+	return results
+}
+
+func buildGraph(equations [][]string,values []float32) (graph map[string]map[string]float32) {
+	for i:=0;i<len(equations);i++{
+		var1:=equations[i][0]
+		var2:=equations[i][1]
+		_,okVar1:=graph[var1]
+		if okVar1{
+			graph[var1][var2]=values[i]
+		}else {
+			graph[var1]=make(map[string]float32)
+			graph[var1][var2]=values[i]
+		}
+	}
+}
+
+
 
--- test/test.go
+++ test/test.go
@@ -1,4 +1,9 @@
 package main
 
+func testmap() (graph map[string]map[string]float32) {
+	graph["ok"]["ok"]=1
+	return graph
+}
 func main() {
+	testmap()
 }
